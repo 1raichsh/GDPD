89858566a1b7e3a405e09c9c0b1dc306babce3de
1raichsh <shilpak.raich-ext@faurecia.com>
2020-03-30 13:03:54 +0200

    Code folder

---
+++ GDPD/MicroStrategyMobile/build/GDPDMicroStrategyMobile.build/Debug-iphonesimulator/MicroStrategyMobileIPhone.build/Objects-normal/x86_64/main 2.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator13.2.sdk/usr/include/libxml2/module.modulemap \
+  /Users/1khurana/Desktop/MobileApp/GDPD/MicroStrategyMobile/MicroStrategyMobileSDK/MicroStrategyMobileSDK_Prefix.pch \
+  /Users/1khurana/Desktop/MobileApp/GDPD/MicroStrategyMobile/OtherSources/main.m
